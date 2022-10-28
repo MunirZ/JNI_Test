1d060c83eb4c52e93beeb8b8d572d4b8cd09f504
MunirZ <102330914+MunirZ@users.noreply.github.com>
2022-10-28 14:54:29 +0200

    Korrektur der Lib-namen

--- src/main/java/HelloWorldCPP.java
+++ src/main/java/HelloWorldCPP.java
@@ -4,9 +4,9 @@ class HelloWorldCPP {
     static {
         String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("windows")) {
-            System.load(System.getProperty("user.dir") + "/build/libs/helloC/shared/helloC.dll");
+            System.load(System.getProperty("user.dir") + "/build/libs/helloCPP/shared/helloCPP.dll");
         } else if (os.contains("linux")) {
-            System.load(System.getProperty("user.dir") + "/build/libs/helloC/shared/libhelloC.so");
+            System.load(System.getProperty("user.dir") + "/build/libs/helloCPP/shared/libhelloCPP.so");
         }
     }
 
